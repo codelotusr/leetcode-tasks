1d6b27c6e4ec4ba7e3330bf7832fc7fda493f733
codelotusr <smaliukasdeividas@gmail.com>
2023-09-19 11:31:45 +0300

    group-anagrams solved

--- Arrays & Hashing/task-49-group-anagrams/49.group-anagrams.cpp
+++ Arrays & Hashing/task-49-group-anagrams/49.group-anagrams.cpp
@@ -8,10 +8,17 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <algorithm>
 
 class Solution
 {
 public:
+    /**
+     * Given an array of strings, group anagrams together.
+     *
+     * @param strs The input array of strings.
+     * @return A vector of vectors of strings, where each inner vector contains a group of anagrams.
+     */
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string> &strs)
     {
         std::vector<std::vector<std::string>> result;
@@ -20,7 +27,16 @@ public:
         for (auto &str : strs)
         {
             std::string key = str;
+            std::sort(key.begin(), key.end());
+            map[key].push_back(str);
         }
+
+        for (auto &pair : map)
+        {
+            result.push_back(pair.second);
+        }
+
+        return result;
     }
 };
 // @lc code=end
