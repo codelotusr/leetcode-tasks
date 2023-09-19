5cd4d9b32b378cb12a8d34262303e85056c9c9e7
codelotusr <smaliukasdeividas@gmail.com>
2023-09-19 11:43:33 +0300

    top-k-frequent-elements added and started

---
+++ Arrays & Hashing/task-347-top-k-frequent-elements/347.top-k-frequent-elements.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=347 lang=cpp
+ *
+ * [347] Top K Frequent Elements
+ */
+
+// @lc code=start
+#include <vector>
+#include <unordered_map>
+
+class Solution
+{
+public:
+    std::vector<int> topKFrequent(std::vector<int> &nums, int k)
+    {
+        std::unordered_map<int, int> count;
+
+        for (const auto &num : nums)
+        {
+            ++count[num];
+        }
+    }
+};
+// @lc code=end
