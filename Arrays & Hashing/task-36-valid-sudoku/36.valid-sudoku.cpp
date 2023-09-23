657696de32226e64c1c38fe3de1b6b03312f13b3
codelotusr <smaliukasdeividas@gmail.com>
2023-09-23 22:05:21 +0300

    valid-sudoku task started

---
+++ Arrays & Hashing/task-36-valid-sudoku/36.valid-sudoku.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=36 lang=cpp
+ *
+ * [36] Valid Sudoku
+ */
+
+// @lc code=start
+#include <vector>
+
+class Solution
+{
+public:
+    bool isValidSudoku(std::vector<std::vector<char>> &board)
+    {
+    }
+};
+// @lc code=end
