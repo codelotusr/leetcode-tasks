86dff746aee9bf23a815747ec2b4bfb497ca33dd
codelotusr <smaliukasdeividas@gmail.com>
2023-09-25 16:39:29 +0300

    solution

--- Arrays & Hashing/task-36-valid-sudoku/36.valid-sudoku.cpp
+++ Arrays & Hashing/task-36-valid-sudoku/36.valid-sudoku.cpp
@@ -6,12 +6,25 @@
 
 // @lc code=start
 #include <vector>
+#include <unordered_map>
 
 class Solution
 {
 public:
     bool isValidSudoku(std::vector<std::vector<char>> &board)
     {
+        std::unordered_map<char, int> row[9], col[9], subBox[9];
+        char c;
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                c = board[i][j];
+                if ((c != '.') && (0 < row[i][c]++ || 0 < col[j][c]++ || 0 < subBox[i / 3 * 3 + j / 3][c]++))
+                    return false;
+            }
+        }
+        return true;
     }
 };
 // @lc code=end
